dbfd8783f7c6168e027b593a1e57525c6c921175
yusufferdogan <yusuffx03@gmail.com>
2024-06-09 15:22:08 +0300

    add methods to ICategoryService

--- Business/Abstract/ICategoryService.cs
+++ Business/Abstract/ICategoryService.cs
@@ -1,10 +1,20 @@
+using Core.Utilities.Results;
 using Entities.Concrete;
 
 namespace Business.Abstract;
 
 public interface ICategoryService
 {
-    List<Category> GetAll();
-    Category GetById(int categoryId);
+    IDataResult<List<Category>> GetAll();
+    IDataResult<Category> GetById(int categoryId);
     
+    IDataResult<Category> GetByName(string categoryName);
+    
+    IResult Add(Category category);
+    
+    IResult Update(Category category);
+
+    IResult Delete(Category category);
+
+
 }
